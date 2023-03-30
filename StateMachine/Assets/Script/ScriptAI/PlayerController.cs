e2530292b20233e4c1ea56edafb7a95f8ca07927
Sarah Barros <capellini.barros@gmail.com>
2023-03-30 16:48:37 -0300

    Payer

--- StateMachine/Assets/Script/ScriptAI/PlayerController.cs
+++ StateMachine/Assets/Script/ScriptAI/PlayerController.cs
@@ -4,31 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float velocidadeParaFrente = 8.0f;
-    [SerializeField] private float velocidadeDeVirada = 100.0f;
 
-    private Rigidbody rb;
 
-    private void Awake()
+    public float moveSpeed = 5f;
+
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
     }
 
-    private void FixedUpdate()
-    {
-        float teclaHorizontal = Input.GetAxis("Horizontal");
-        float teclaVertical = Input.GetAxis("Vertical");
 
-        // Não permite que o jogador ande para trás.
-        if (teclaVertical < 0.0f)
-        {
-            teclaVertical = 0.0f;
-        }
-
-        // Movimenta o jogador na direção para frente e para trás.
-        rb.velocity = transform.forward * teclaVertical * velocidadeParaFrente;
-
-        // Rotaciona o jogador na direção horizontal.
-        transform.Rotate(Vector3.up * teclaHorizontal * velocidadeDeVirada * Time.deltaTime);
-    }
 }
